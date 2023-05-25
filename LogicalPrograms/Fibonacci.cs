44528bcf49d2569ce2f03efa4bc036018628cfb9
“Akhilkumar1f4” <akhilkumar1f4@gmail.com>
2023-05-25 12:47:35 +0530

    Add:logical problems added

---
+++ LogicalPrograms/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class FibonacciGenerator
+{
+    private int previous;
+    private int current;
+
+    public FibonacciGenerator()
+    {
+        previous = 0;
+        current = 1;
+    }
+
+    public int Next()
+    {
+        int next = previous + current;
+        previous = current;
+        current = next;
+        return current;
+    }
+}
---
+++ LogicalPrograms/Perfect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PerfectNumberChecker
+{
+    public static bool IsPerfectNumber(int number)
+    {
+        int sumOfDivisors = CalculateSumOfDivisors(number);
+        return sumOfDivisors == number;
+    }
+
+    private static int CalculateSumOfDivisors(int number)
+    {
+        int sum = 0;
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+                sum += i;
+        }
+        return sum;
+    }
+}
